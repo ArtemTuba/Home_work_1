3b44f5642ecf54c8a168f7aca768a43fb406bc9a
ArtemTuba <tuba9521@gmail.com>
2022-12-07 12:27:09 +0200

    added home work 1 fix

---
+++ main.js
@@ -0,0 +1,10 @@
+const first = +prompt('Введіть перше число :)');
+console.log(first);
+
+const second = +prompt('Введіть друге число :)');
+console.log(second);
+
+alert(`Додавання: ${first} + ${second} = ${first + second}`);
+alert(`Віднімання: ${first} - ${second} = ${first - second}`);
+alert(`Множення: ${first} * ${second} = ${first * second}`);
+alert(`Ділення: ${first} / ${second} = ${first / second}`);
